fe2b81f0be8b17709a9058f54eb4e05fc5285ca6
j-novaes <joaovictor.nb21@hotmail.com>
2023-02-16 14:59:38 +0000

    Remade App as a functiona component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState } from 'react';
+import SearchBar from './search-bar/search-bar.component';
+import CardList from './card-list/card-list.component';
 
-function App() {
+const App = () => {
+
+  const [monsters, setMonsters] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        setMonsters(users)
+      )
+  }, [])
+
+  useEffect(() => {
+    setFilteredMonsters(monsters.filter(m => {
+      return m.name.toLowerCase().includes(search.toLowerCase());
+    }))
+  }, [monsters,search])
+  
+
+  const onSearchChange = (event) => {
+    setSearch(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBar
+        classes=""
+        searchChangeHandler={onSearchChange}
+        placeholder="Search monster by name"
+      />
+      <CardList monsters={filteredMonsters} />
+    </main>
+  )
 }
 
+/*
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      search: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState({ monsters: users })
+      )
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ search: event.target.value });
+  }
+  render() {
+
+    const { monsters, search } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(m => {
+      return m.name.toLowerCase().includes(search.toLowerCase());
+    })
+
+    return (
+      <main>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBar
+          classes=""
+          searchChangeHandler={onSearchChange}
+          placeholder="Search monster by name"
+        />
+        <CardList monsters={filteredMonsters} />
+      </main>
+    )
+  }
+}
+*/
 export default App;
